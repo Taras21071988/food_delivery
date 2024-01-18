7b4a0f7d67d395881ce7a3012e5fe122383a4315
Taras21071988 <tsasojlov@icloud.com>
2024-01-18 11:46:09 +0200

    creating an "accordion" for the description in the faq block

--- src/js/index.js
+++ src/js/index.js
@@ -19,3 +19,17 @@ const swiper = new Swiper(".swiper", {
     prevEl: "#sliderPrev",
   },
 });
+
+var panelItem = document.querySelectorAll(".faq__descr-item"),
+  active = document.getElementsByClassName("activePanel");
+
+Array.from(panelItem).forEach(function (item, i, panelItem) {
+  item.addEventListener("click", function (e) {
+    if (active.length > 0 && active[0] !== this)
+      // если есть активный элемент, и это не тот по которому кликнули
+      active[0].classList.remove("activePanel"); // убрать класс panel-active
+
+    // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
+    this.classList.toggle("activePanel");
+  });
+});
