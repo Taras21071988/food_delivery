f708d8383ad6617168190a0a4fd791c7c632e400
Taras21071988 <tsasojlov@icloud.com>
2024-01-18 14:33:05 +0200

    small code refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -21,20 +21,6 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-// var panelItem = document.querySelectorAll(".faq__descr-item"),
-//   activeq = document.getElementsByClassName("activePanel");
-
-// Array.from(panelItem).forEach(function (items, i, panelItem) {
-//   items.addEventListener("click", function (e) {
-//     if (activeq.length > 0 && activeq[0] !== this)
-//       // если есть активный элемент, и это не тот по которому кликнули
-//       activeq[0].classList.remove("activePanel"); // убрать класс panel-active
-
-//     // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
-//     this.classList.toggle("activePanel");
-//   });
-// });
-
 // var panelbtn = document.querySelectorAll(".faq__chapter-items"),
 //   active = document.getElementsByClassName("activ");
 
@@ -50,5 +36,4 @@ const swiper = new Swiper(".swiper", {
 //   });
 // });
 
-
-panAct()
+panAct();
