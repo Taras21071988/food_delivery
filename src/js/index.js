9d5c170036b252bca54dd1653173ef8cdc5304a1
Taras21071988 <tsasojlov@icloud.com>
2024-01-18 16:33:30 +0200

    writing logic for tabs and changing the logic of "accordeon"

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,57 @@
-import panAct from "./modules/activePanel";
-import swiper from "./modules/swiper";
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
 
-// var panelbtn = document.querySelectorAll(".faq__chapter-items"),
-//   active = document.getElementsByClassName("activ");
+// import styles bundle
+import "swiper/css/bundle";
 
-// Array.from(panelbtn).forEach(function (item, i, panelbtn) {
-//   item.addEventListener("click", function (e) {
-//     if (active.length > 0 && active[0] !== this)
-//       // если есть активный элемент, и это не тот по которому кликнули
-//       active[0].classList.remove("activ"); // убрать класс panel-active
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: "#sliderNext",
+    prevEl: "#sliderPrev",
+  },
+});
 
-//     // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
-//     this.classList.add("activ");
+let panelItem = document.querySelectorAll(".faq__descr-item");
+let active = document.getElementsByClassName("activePanel");
 
-//   });
-// });
-swiper;
-panAct();
+Array.from(panelItem).forEach(function (items, i, panelItem) {
+  items.addEventListener("click", function (e) {
+    if (active.length > 0 && active[0] !== this)
+      active[0].classList.remove("activePanel");
+    this.classList.toggle("activePanel");
+  });
+});
+
+const showTab = (elTabBtn) => {
+  const elTab = elTabBtn.closest(".faq__wrapper");
+  if (elTabBtn.classList.contains("activ")) {
+    return;
+  }
+  const targetId = elTabBtn.dataset.targetId;
+  const elTabPane = elTab.querySelector(`.faq__descr[data-id="${targetId}"]`);
+  if (elTabPane) {
+    const elTabBtnActive = elTab.querySelector(".activ");
+    elTabBtnActive.classList.remove("activ");
+    const elTabPaneShow = elTab.querySelector(".selected");
+    elTabPaneShow.classList.remove("selected");
+    elTabBtn.classList.add("activ");
+    elTabPane.classList.add("selected");
+  }
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target && !e.target.closest(".faq__chapter-items")) {
+    return;
+  }
+  const elTabBtn = e.target.closest(".faq__chapter-items");
+  showTab(elTabBtn);
+});
